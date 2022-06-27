ba26c3155d12edf2ff1df0152ed8dee5eb3223de
henrisama <pedropiresflores@icloud.com>
2022-06-26 21:31:41 -0400

    set api url 2

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";  
 
 export const api = axios.create({
-    baseURL: 'http://ec2-18-231-121-38.sa-east-1.compute.amazonaws.com:4000/'
+    baseURL: 'ec2-18-231-121-38.sa-east-1.compute.amazonaws.com:4000'
 });
